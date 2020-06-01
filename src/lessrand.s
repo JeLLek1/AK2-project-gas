9a2fffa17d45b87461de908f1991f56ebb6b8a70
danbulgar <218192@student.pwr.edu.pl>
2020-06-01 16:00:56 +0200

    Random number

---
+++ src/lessrand.s
@@ -0,0 +1,81 @@
+# losowa liczb na podanej liczbie dword
+# losuje kolejne czlony za pomoca random.s (seed wziety za pomoca srand.s)
+# potem dzieli przez podany dzielnik (tutaj wprost podany, a docelowo powinien byc z arg)
+
+.section .data
+randomLength: .long 3
+.equ restTempPtr, -4
+.equ denomTempPtr, -8
+.equ randomStartPtr, -12
+.section .bss
+.lcomm data, 4
+
+.section .text
+
+.global lessrand
+
+.type lessrand, @function
+lessrand:
+    pushl %ebp
+    movl %esp, %ebp
+    subl $16, %esp
+  
+    pushl %edi              # save local register
+    pushl %esi              # save local register
+    pushl %ebx              # save local register
+
+    #alokacja pamięci dla reszty z dzielenia
+	movl randomLength, %eax
+	shll $2, %eax				#długość w bajtach
+	pushl %eax				#argumnet funkcji
+	call allocate				#funkcja alokacji
+	addl $4, %esp				#zdjęcie argumentu
+	movl %eax, restTempPtr(%ebp)		#wynik funkcji
+	#==========================================
+
+	#alokacja pamięci dla liczby ikrementowanej
+	movl randomLength, %eax
+	shll $2, %eax				#długość w bajtach
+	pushl %eax				#argumnet funkcji
+	call allocate				#funkcja alokacji
+	addl $4, %esp				#zdjęcie argumentu
+	movl %eax, denomTempPtr(%ebp)	#wynik funkcji
+	#==========================================
+
+    #alokacja pamięci dla liczby ikrementowanej
+	movl randomLength, %eax
+	shll $2, %eax				#długość w bajtach
+	pushl %eax				#argumnet funkcji
+	call allocate				#funkcja alokacji
+	addl $4, %esp				#zdjęcie argumentu
+	movl %eax, randomStartPtr(%ebp)	#wynik funkcji
+	#==========================================
+
+    movl denomTempPtr(%ebp), %eax
+    movl $2000, 4(%eax)
+
+    call srand_vec
+    pushl randomLength
+	pushl randomStartPtr(%ebp)
+    call random_vec
+    addl $8, %esp
+
+	pushl randomLength
+    pushl randomStartPtr(%ebp)
+    pushl randomLength
+	pushl restTempPtr(%ebp)
+	pushl randomLength
+	pushl denomTempPtr(%ebp)
+	call bindiv # - tu twoja funkcja
+	addl $20, %esp
+aft:	
+    movl restTempPtr(%ebp), %eax
+
+func_exit:
+    popl %ebx
+    popl %esi
+    popl %edi
+
+    movl %ebp, %esp			#odtworzenie starego stosu
+	popl %ebp
+    ret
---
+++ src/random.s
@@ -0,0 +1,58 @@
+#generowanie liczb pseudolosowych wektora
+#sposób LCG
+#
+#Dane:
+# seed - zmienna globalna przechowująca aktualny seed losowania
+#
+#argumenty funckji
+# vector - wskaźnik na początek wektora
+#
+#stałe
+#
+# randA - do liczby pseudolosowej rand = randA*seed+randC
+# randC - do liczby pseudolosowej rand = randA*seed+randC
+
+.section .data
+	.equ vector, 8
+	.equ randA, 1103515245
+	.equ randC, 12345
+.section .text
+
+.global random_vec, seed
+
+.type random_vec, @function
+random_vec:
+	pushl %ebp
+	movl %esp, %ebp				#prolog funkcji 
+	subl $8, %esp
+	pushl %edi              # save local register
+    pushl %esi              # save local register
+    pushl %ebx              # save local register
+
+	movl 12(%ebp), %eax
+	movl %eax, -4(%ebp)
+
+	movl seed, %eax				#pobranie aktualnego seedu
+
+	movl $0, %esi				#iteracja pętli
+random_loop:
+	movl $randA, %edx			#randA jako argument mnożenia
+	mull %edx				#mnożenie razy randA (radA*seed+randC)
+	addl $randC, %eax  			#dodanie randC
+
+	xorl %edx, %edx				#druga część dzielnej jest 0
+	movl vector(%ebp), %ebx			#wkskaźnik na wektor
+	movl %eax, (%ebx, %esi, 4)		#przeniesienie wyniku do odpowiedniej pozycji wektora
+
+	incl %esi
+	cmpl -4(%ebp), %esi
+	jb random_loop				#wykonaj dla wszystkich pozycji wektora
+
+	movl %eax, seed 			#nowy seed do pamięci
+
+	popl %ebx
+    popl %esi
+    popl %edi
+	movl %ebp, %esp				#odtworzenie starego stosu
+	popl %ebp
+	ret
